ecc8837cdb30f68baa63699bd25f7fcfc2e53588
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2025-07-14 18:01:40 +0545

    Resume

---
+++ Basic/test.py
@@ -0,0 +1,22 @@
+
+
+
+#WAP to ask the name, age and Address & print it
+
+#string input
+name= input("Enter your Name : ")
+
+#integer input
+age= int(input("Enter your age: "))
+
+#string input
+address= input("Enter your address: ")
+
+#printing
+
+print("Name:  ",name)
+print("Age: ",age)
+print("Address: ", address)
+
+
+
