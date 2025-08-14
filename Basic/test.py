1de614ff31d11fed23438abae7fee8997cb0a431
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2025-08-14 09:57:17 +0545

    Hey there

--- Basic/test.py
+++ Basic/test.py
@@ -1,39 +1,5 @@
-# # # # # #string input
- 
-# # # # # EmpNam= input("Enter the employee name: ")
 
-# # # # # print("The employee name is : ", EmpNam)
+# # #getting started
 
-# # # # # #using f string
-# # # # # print(f"Employee name : {EmpNam}")
-
-# # # # #multiple input
-
-# # # # c1, c2=input("Enter two codes: ").split()
-
-# # # # # c1=int(c1)
-# # # # # c2= int(c2)
-
-# # # # print(f"{c1} is first code and {c2} is second code.")
-
-# # # x= input('Enter somthing: ')
-# # # print(x)
- 
-
-# # #print in same line 
-
-# # print("he, there i am",end="")
-# # print("Priyanshu Singh")
-
-# # #adding separater
-
-# print("hi","I","am","Munna",sep="")
-
-#enter the no. in float and print in 2 decimal value
-
-x=float(input("Enter the pie value: "))
-
-print(f"the calculated value is {x:.2f}")
-
-print(type(x))
-
+# # print("Back to track")
+# print("Hello there..I am getting started :")
