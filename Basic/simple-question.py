71d1271a5e4e5fbc58b030c1749634f9d6e22a66
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-03-26 15:12:16 +0545

    Started

---
+++ Basic/simple-question.py
@@ -0,0 +1,28 @@
+#print hello world
+
+print("Hello world")
+
+#take 2, Take two numbers and print
+
+# a= int(input("Enter the first number: "))
+# b= int(input("Enter the 2nd number:"))
+# print("the sum is", a+b)
+
+#take the age and print" after 5 years you will be..
+
+# age= int(input("Enter your current age: "))
+# print("your current age is", age)
+# print (" after 5 years, your age will be", age + 5)
+
+# take name and age , print(my name is and i am -- years old)
+
+# name= input(" enter your name: ")
+# age= int(input( "enter your age"))
+
+# print(f"your name is {name} and you are {age} years old")
+
+#Take a number and print double and square
+
+a = int (input(" enter a number:"))
+print ("Double is", a+ a)
+print ("Square is", a*a)
--- Basic/test.py
+++ Basic/test.py
@@ -1,6 +1,49 @@
-# string input'
+# input & print
+# name= "Priyanshu "
+# age= 23
+# add= "KTM"
 
-name= input("What's your name? ")
+# # print(name)
 
-print("Your name is ", name)
+# # print(age)
 
+# # print("My name is", name)
+# # print("My age is",age)
+
+# #by formating
+
+# print(f"My name is {name} and my age is {age}")
+# """
+# #input string
+
+# # name= input("enter your name")
+# # print(name)
+
+# interger input
+
+# add= input("enter your address")
+# print(add)
+
+# age= int(input("Enter your age"))
+# print (age)
+
+#sum
+# a= int(input("Enter first namber: "))
+# b= int(input("Enter the Second number: "))
+
+# print(a + b )
+
+# price= float(input(" enter the price: "))
+# print(price)
+
+# #take current age and display his age of next year
+
+# age= int(input("Enter your current age "))
+
+# print("next year your age will be", age+ 1)
+
+# take a nubmber and print the square of that number
+
+a= int(input("enter number: "))
+print("Square is", a*a)
+print(f"Square is {a*a}")
