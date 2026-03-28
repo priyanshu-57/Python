49b22778f3f8d47424ec9925950c3508227578d7
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-03-28 14:21:17 +0545

    resume

--- Basic/test.py
+++ Basic/test.py
@@ -1,68 +1,20 @@
-# input & print
-# name= "Priyanshu "
-# age= 23
-# add= "KTM"
 
-# # print(name)
 
-# # print(age)
+# print("hey there")
 
-# # print("My name is", name)
-# # print("My age is",age)
-
-# #by formating
-
-# print(f"My name is {name} and my age is {age}")
-# """
-# #input string
-
-# # name= input("enter your name")
-# # print(name)
-
-# interger input
-
-# add= input("enter your address")
-# print(add)
-
-# age= int(input("Enter your age"))
-# print (age)
-
-#sum
-# a= int(input("Enter first namber: "))
-# b= int(input("Enter the Second number: "))
-
-# print(a + b )
-
-# price= float(input(" enter the price: "))
-# print(price)
-
-# #take current age and display his age of next year
-
-# age= int(input("Enter your current age "))
-
-# print("next year your age will be", age+ 1)
-
-# take a nubmber and print the square of that number
-
-# a= int(input("enter number: "))
-# print("Square is", a*a)
-# print(f"Square is {a*a}")
-
-# print("Hi i am  munna")
-# a= 28
+# a= 10
 # print(a)
 
-# a="Ruby"
-# b="julain"
-# print(f"{a} is stronger than {b} in team fights")
+# a=20
+# b=23
 
-#input
-# name= input("what your name: ")
-# print(name)
+# print(f"She is {a} years old and he is {b} years old")
 
-# a= float(input("Enter your percentage:"))
-# print(a)
+# name= input("Whats your name: ")
+# print("Name:", name)
 
-print(type(23))
-print(type(2.5))
-print(type("hey"))
+# age= int(input("Whats your age:"))
+# print("age:", age)
+
+l= float(input("its length:"))
+print("length:",l)
