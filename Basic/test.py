5c29453bb2b9e4ba2923d93d03203b0cabdc7c79
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-04-12 17:08:03 +0545

    Updated

--- Basic/test.py
+++ Basic/test.py
@@ -1,30 +1,15 @@
 
-#prime no .check
-
-# num= int(input("Enter the no.:"))
-
-# if num<= 1:
-#     print("Not Prime Number")
-# else:
-#     for i in range(2, int(num**0.5) +1):
-#         if num % i == 0:
-#             print("Not prime no.")
-#             break
-#     else:
-#         print("Prime number")
-
-
 #palindrome no.
 
-num= int(input("Enter the no. :"))
+num= int(input("Enter the no.:"))
 rev=0
-original= num
+orginal=num
 while num > 0:
     digit= num % 10
-    rev = rev *10 + digit
+    rev= rev*10 + digit
     num= num//10
 
-if original==rev:
-    print("Palindrome")
+if orginal== rev:
+    print("Palindrome no.")
 else:
-    print("not palindrome")
+    print("Not palindrome no.")
--- Dictionaries/revise.py
+++ Dictionaries/revise.py
@@ -1,11 +1,38 @@
 
-mark= [{"A": "20"},{"B" : "30"}, {"c":"40"}]
+# price=[600,700,1000,1500,2000,2500]
 
-# mark= {"A": "20","B" : "30" , "c":"40"}
+# # print(price)
 
+# price.append(1750)
+# price.remove(700)
 
-# print(mark["A"])
+# # print(price)
 
-# print(mark[0])
+# for i in price:
+#     print(i)
 
-print(mark["A"])
+#Dictionaries
+
+# d= {"name":"Ram","age":"20","Height":"2 feet"}
+
+# d["name"]= "Priyanshu "
+# d["id"]= 40
+
+# print(d)
+
+#list of dictionaries
+# mark= [
+#     {'name ': 'A','mark':'40'},
+#     {'name':'B','mark':'50'},
+#     {'name':'C','mark':'45'}
+# ]
+
+# detail={'name':'A','id':'2', }
+# print(detail["name"])
+
+# detail["name"]= "Ram" #update
+# detail["mark"]= 25
+
+# print(detail["name"])
+# print(detail["mark"])
+
