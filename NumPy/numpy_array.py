c4230a8902e09ac972aa981bbc968e64a4f8d9be
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-04-17 20:17:18 +0545

    revised all

--- NumPy/numpy_array.py
+++ NumPy/numpy_array.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-arr = np.array([10,20,30,40])
+arr = np.array([10,20,30,40]) #convert normal List to numpy array
 print(arr)
 
 print(type(arr))
--- Pandas/revise.py
+++ Pandas/revise.py
@@ -59,14 +59,131 @@
 
 #list of dictionaries
 
-cars= [{'name': 'BYD','range': '500','TopSpeed': '200'},
-       {'name': 'Tesla','range': '1000','TopSpeed': '150'},
-       {'name': 'KIA','range': '600','TopSpeed': '120'}
-       ]
+# cars= [{'name': 'BYD','range': '500','TopSpeed': '200'},
+#        {'name': 'Tesla','range': '1000','TopSpeed': '150'},
+#        {'name': 'KIA','range': '600','TopSpeed': '120'}
+#        ]
 
 #kia topspeed
 
-for i in cars:
-    if i["name"] == "KIA":
-        print(i["TopSpeed"])
+# for i in cars:
+#     if i["name"] == "KIA":
+#         print(i["TopSpeed"])
    
+
+#file handling
+
+# with open("revise.txt","w") as f:
+
+#     for i in cars:
+#         #available models
+#         f.write(f"Avaiable models: {i['name']} \n")
+
+#read file
+
+# with open("revise.txt","r") as f:
+#     data= f.read()
+#     print(data)
+
+#read line by line
+
+# with open("revise.txt", "r") as f:
+#     for line in f:
+#         print(line.strip())
+
+#numpy
+
+#array
+# import numpy as np
+
+# arr= np.array([1,2,3,4,5]) #convert normal List to numpy array
+
+# print(arr)
+
+# #basic operation
+
+# print(arr.mean())
+# print(arr.max())
+# print(arr.min())
+
+# #arithmetic operation
+
+# print(arr + 10)
+
+# print(arr * 10)
+
+# import numpy as np
+
+# prime= np.array([2,3,5,7])
+
+# print(prime)
+
+# print(prime.mean())
+
+# print(prime.max())
+
+# print(prime.min())
+
+# print(prime * 10)
+
+import pandas as pd
+
+data= {
+    "name": ["A","B","C"],
+    "marks": [80,60,40]}
+
+x= pd.DataFrame(data)
+
+# print(x)
+
+# print(x.head())
+# print(x.describe())
+
+#column access
+
+# print(x["name"])
+
+#filtering
+# print(x[x["marks"]>=50])
+
+# #add new column
+# x['result']= x["marks"]>=50
+
+# print(x)
+
+# print(x["result"].value_counts())
+
+# import numpy as np
+
+# x= np.array([1,2,3,4,5])
+
+# print(x)
+
+# print(x.mean())
+# print(x.max())
+# print(x.min())
+
+# print(x+10)
+
+import pandas as pd
+
+data= {
+    "name": ["A", "B", "c"],
+    "mark": [50,40,90]
+
+}
+
+x= pd.DataFrame(data)
+
+# print(x)
+
+# print(x.head())
+# print(x.describe())
+
+# print(x["name"])
+
+# print(x[x["mark"]>=50])
+
+x["result"]= x["mark"]>=50
+
+print(x["result"].value_counts())
