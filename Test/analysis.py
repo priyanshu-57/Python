aa7285662c78cef0fd1f6d6f9867a7bc7fab98f2
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-04-25 10:02:09 +0545

    new day

--- Test/analysis.py
+++ Test/analysis.py
@@ -1,72 +1,11 @@
 
-# student= {
-#     'name': 'A',
-#     'mark': '56',
-#     'id': '101'
-# }
+# with open("new.txt","w") as f:
+#     f.write("hey there\n I am Priyanshu.")
 
-# #add
-# student['Grade']= 'C'
-
-# student['mark']= 95
-# # print(student)
-
-# #list of dictionaries
-
-# students= [
-#     {'name':'A', 'mark': '75'},
-#     {'name': 'B', 'mark':'56'},
-#     {'name':'C', 'mark':'85'}
-# ]
-# #a=75, b=56
-
-# for i in students[:2]:
-#     print(f"{i['name']}= {i['mark']}")
-
-#write file
-
-# with open("revise.txt","w") as f:
-#     f.write("My name is Priyanshu")
-
-#read
-# with open("revise.txt","r") as f:
+# with open("new.txt","r") as f:
 #     content= f.read()
 #     print(content)
 
-# #read line
-# with open("revise.txt","r") as f:
+# with open("new.txt","r") as f:
 #     for line in f:
-#         print(line.strip())
-
-#numpy
-
-# import numpy as np
-
-# arr=np.array([1,2,3,4,5])
-
-# print(arr)
-# print(arr.mean())
-# print(arr.max())
-# print(arr.min())
-
-#pandas
-import pandas as pd
-
-student={
-    "name": ["A","B"],
-    "marks" : [45, 95]
-}
-
-df= pd.DataFrame(student)
-# print(df)
-
-# print(df.head())
-# print(df.describe())
-# print(df.columns)
-
-# print(df[df['marks']>60])
-
-df["result"]= df["marks"]>50
-
-print(df)
-print(df["result"].sum())
+#         print(f"{line}**")
