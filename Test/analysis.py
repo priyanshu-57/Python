a00cb38e086bdd7473be96067dd89cd55559c3d9
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-05-07 09:11:20 +0545

    resume

--- Test/analysis.py
+++ Test/analysis.py
@@ -1,29 +1,4 @@
 
-#create CSV file
-
-# with open("table.csv", "w") as f:
-#     f.write("Name, marks\n A,40\n B,65\n C,90")
-
-import pandas as pd
-
-df= pd.read_csv("table.csv")
-df.columns=df.columns.str.strip()
-
-# print(df)
-
-
-def result(mark):
-    if mark >=50:
-        return "pass"
-    else:
-        return "fail"
-
-df["Result"]= df["marks"].apply(result)
-
-
-
-print("Average:",df["marks"].mean())
-print("Highest:",df["marks"].max())
-
-print(df)
-print(df["Result"].value_counts())
+a= 10.5
+ 
+print(int(a))
