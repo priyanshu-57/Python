7658270cde5140a0942b61ea3fec5baf0814af43
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-05-14 12:04:34 +0545

    Resume

--- Test/analysis.py
+++ Test/analysis.py
@@ -1,4 +1,24 @@
 
-a= 10.5
- 
-print(int(a))
+# print("hello")
+
+# a= 4.5
+# print(type(a))
+
+
+# for i in range(0,6):
+    
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print()
+
+# a= int(input("Enter the no:"))
+# b= int(input("Enter 2nd no:"))
+
+# def add(x,y):
+#     total= x+y
+#     print("Total",total)
+
+# add(a,b)
+
+
+
