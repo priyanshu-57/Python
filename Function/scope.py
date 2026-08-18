24e96ccc18d0a39d992ef9613bea2ae1e8257101
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-08-19 00:46:25 +0545

    scope all

--- Function/scope.py
+++ Function/scope.py
@@ -7,7 +7,7 @@
 
 # my_func()
 
-# print(my_var) #its local scope
+# print(my_var) #its local scope ,NameError: name 'my_var' is not defined
 
 #enclosing Scope
 
@@ -23,9 +23,34 @@
 
 #example
 
-def outer_fn():
-    msg="Hello" #msg is in enclosing scope
-    def inner_fn():
-        print(msg)
-    inner_fn()
-outer_fn()
+# def outer_fn():
+#     msg="Hello" #msg is in enclosing scope
+#
+#     def inner_fn():
+#         print(msg)
+#     inner_fn()
+#
+# outer_fn()
+
+# #Global scope
+
+# my_var= 1
+
+# def show_var():
+#     print(my_var)
+
+# show_var()
+
+# using "global keyword"
+
+my_var_1= 2
+
+def show_var():
+    global my_var_2
+    my_var_2= 6
+    print(my_var_1)
+    print(my_var_2)
+
+show_var()
+print(my_var_2)
+
